5e2186fbd60bc4e85d7fa3c5090361d0d198b553
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2021-02-04 12:09:15 +0300

    added examples with 'is' and result listing

--- lesson_1/task_1.py
+++ lesson_1/task_1.py
@@ -76,6 +76,7 @@ print(_expression.format(title='Логические операции', marker=_
 
 print(f'Напоминаем, что для сравнения вы используете:\na = {_a}\nb = {_b}\n')
 _expression_third = "{:<20} = {}"
+_instance = int(5)
 _box_new: list = [
     ('a > b', _a > _b),
     ('a < b', _a < _b),
@@ -93,9 +94,81 @@ _box_new: list = [
     ('a is 100', _a is 100),
     ('a is not 100', _a is not 100),
     ('100 is a', 100 is _a),
+    ('instance = int(5)', _instance),
+    ('instance == 5', _instance == 5),
+    ('instance is 5', _instance is 5),
+    ('instance is int()', _instance is int()),
+    ('instance is int', _instance is int),
+    ('instance is instance', _instance is _instance)
 ]
 
 for values in _box_new:
     print(_expression_third.format(*values))
 
-print(_expression.format(title='Арифметические операции', marker=_marker))
+
+# Это результат вывода при запуске скрипта
+"""
+***** Список зарезервированных слов *****
+
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 
+'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 
+'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 
+'raise', 'return', 'try', 'while', 'with', 'yield']
+
+***** Преобразование и проверка типов *****
+
+int(5) = 5
+int(5.87) = 5
+float(5.87) = 5.87
+bool(1) = True
+bool(0) = False
+bool('') = False
+bool('Я текст') = True
+type(5) = <class 'int'>
+type(5.55) = <class 'float'>
+type('5') = <class 'str'>
+type(True) = <class 'bool'>
+
+***** Арифметические операции *****
+
+Вы используете:
+a = 100
+b = 5.5
+
+a + b      = 105.5
+a - b      = 94.5
+a * b      = 550.0
+a / b      = 18.181818181818183
+a // b     = 18.0
+a % b      = 1.0
+a ** b     = 100000000000.0
+
+***** Логические операции *****
+
+Напоминаем, что для сравнения вы используете:
+a = 100
+b = 5.5
+
+a > b                = True
+a < b                = False
+a == b               = False
+a != b               = True
+a >= b               = True
+a <= b               = False
+bool(a and b)        = True
+bool(a and None)     = False
+bool(a or b)         = True
+bool(not a or b)     = True
+bool(not a or not b) = False
+a in [1, 2, 3]       = False
+a not in [1, 2, 3]   = True
+a is 100             = True
+a is not 100         = False
+100 is a             = True
+instance = int(5)    = 5
+instance == 5        = True
+instance is 5        = True
+instance is int()    = False
+instance is int      = False
+instance is instance = True
+"""

9674ecd80491780016983a86d36ce22ea29fd7e4
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2021-02-18 10:46:33 +0300

    done task 4 for lesson 4

--- lesson_4/task_4.py
+++ lesson_4/task_4.py
@@ -7,3 +7,18 @@
 Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 Результат: [23, 1, 3, 10, 4, 11]
 """
+
+source_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+result_list = [_ for _ in source_list if source_list.count(_) == 1] 
+result_list2 = list(filter(lambda x: source_list.count(x) == 1, source_list))
+
+print('{:<20}: {}'.format('Исходный список', source_list))
+print('{:<20}: {}'.format('Результат', result_list))
+print('{:<20}: {}'.format('filter результат', result_list2))
+
+"""
+Результат выполнения:
+Исходный список     : [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+Результат           : [23, 1, 3, 10, 4, 11]
+filter результат    : [23, 1, 3, 10, 4, 11]
+"""

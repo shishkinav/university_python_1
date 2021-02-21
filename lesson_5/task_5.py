be5d10677386ce5365d40f12886df20f1ea1c6ff
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2021-02-22 00:37:12 +0300

    done task 5 for lesson 5

--- lesson_5/task_5.py
+++ lesson_5/task_5.py
@@ -3,3 +3,20 @@
 набор чисел, разделенных пробелами. Программа должна подсчитывать 
 сумму чисел в файле и выводить ее на экран.
 """
+from random import random
+
+
+try:
+    # генерируем 50 случайных float значений и записываем в файл через пробел
+    with open('task_5_result.txt', 'w', encoding='utf-8') as fw:
+        fw.writelines(
+            [f"{round(random() * (15000 - 5000) + 5000, 2)} " for _ in range(50)]
+        )
+    # считываем значения и суммируем
+    with open('task_5_result.txt') as fr:
+        s = sum(
+            [float(number) for number in fr.read().split()]
+        )
+    print(f"Сумма чисел в файле равна {round(s, 2)}")
+except Exception as err:
+    print(f"Ошибка: {err}")

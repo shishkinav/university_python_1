d980a1d1ab2e8ecf57396500885a686a5a2e998f
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2021-02-21 23:39:25 +0300

    done task 1 for lesson 5

--- lesson_5/task_1.py
+++ lesson_5/task_1.py
@@ -3,3 +3,10 @@
 построчно данные, вводимые пользователем. Об окончании ввода 
 данных свидетельствует пустая строка.
 """
+with open('task_1_text.txt', 'w', encoding='utf-8') as file:
+    while True:
+        row_from_user = input("Введите строку для записи, "
+                              "либо оставьте строку пустой для остановки:\n")
+        if not row_from_user:
+            break
+        file.write(f'{row_from_user}\n')

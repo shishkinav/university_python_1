6d7df2a6c0f57e08335a9936785c7cf235cc98e7
Anatoliy Shishkin <shishkinanatoli@gmail.com>
2021-02-25 15:24:52 +0300

    done task 5 for lesson 6

--- lesson_6/task_5.py
+++ lesson_6/task_5.py
@@ -9,3 +9,58 @@
 Создать экземпляры классов и проверить, что выведет описанный метод 
 для каждого экземпляра.
 """
+from abc import ABC, abstractmethod
+
+
+class Stationery(ABC):
+    """Абстрактный класс канцелярской принадлежности"""
+    title: str = ''
+
+    @abstractmethod
+    def draw(self) -> None:
+        """Метод отрисовки"""
+        print(f'Запуск отрисовки {self.title.capitalize()}')
+
+
+class Pen(Stationery):
+    title: str = 'ручка'
+
+    def draw(self) -> None:
+        super().draw()
+        print("I'm pen.")
+
+
+class Pencil(Stationery):
+    title: str = 'карандаш'
+
+    def draw(self) -> None:
+        super().draw()
+        print("I'm pencil.")
+
+
+class Handle(Stationery):
+    title: str = 'маркер'
+
+    def draw(self) -> None:
+        super().draw()
+        print("I'm handle.")
+
+
+if __name__ == '__main__':
+    try:
+        for stationery in [Pen(), Pencil(), Handle()]:
+            stationery.draw()
+    except Exception as err:
+        print(f'Ошибка: {err}')
+
+"""
+Результат выполнения:
+
+> python task_5.py 
+Запуск отрисовки Ручка
+I'm pen.
+Запуск отрисовки Карандаш
+I'm pencil.
+Запуск отрисовки Маркер
+I'm handle.
+"""
